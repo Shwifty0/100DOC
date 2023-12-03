fb7a88215a57b2e0e8ab752725dda9eed02a8a0a
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2023-12-03 21:00:50 +0500

    grinding

--- 100DOCDay25/dsa.py
+++ 100DOCDay25/dsa.py
@@ -28,4 +28,37 @@ def bubble_sort(lst):
                 sort_list = lst
     return sort_list
 
-print(bubble_sort(lst))
+# TODO
+# Palindrome
+str = 'racecar'
+
+def palindrome(str):
+    return str[-1::-1]
+
+#print(palindrome(str))
+
+# TODO
+# Check if the sequence is in the list
+
+lst1 = [1,2,3,4,5]
+lst2 = [2,3]
+
+def check(lst1, lst2):
+    index = []
+    for num in lst2:
+        if num in lst1:
+            index.append(lst1.index(num))
+    return index
+
+#print(check(lst1, lst2))
+
+# TODO
+# Swapping 
+a = 20
+b = 30
+
+a = a + b
+b = a - b
+a = a - b
+
+print(a, b)
