eb1756e166aefebb594315b4b25a2437315b7461
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2023-12-20 05:48:36 +0500

    Password Generator GUI Complete

--- 100DOCDay29/main.py
+++ 100DOCDay29/main.py
@@ -1,8 +1,38 @@
 from tkinter import *
-
+import tkinter.messagebox
+from random import choice, randint, shuffle
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    
+    password_letters = [choice(letters) for char in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for char in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for char in range(randint(2, 4))]
 
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+
+    pw_entry.delete(0, END)
+    pw_entry.insert(0, password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = web_entry.get()
+    email = email_entry.get()
+    password = pw_entry.get()
+    data = f"Website:{website} | email: {email} | password:{password}\n"
+
+    with open('data.txt', 'a') as f:
+        f.writelines(data)
+        web_entry.delete(0,'end')
+        pw_entry.delete(0,'end')
+
+    tkinter.messagebox.showinfo("Confirmation", message="Password has been saved!")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -11,32 +41,33 @@ window.title('Password Manager')
 
 canvas = Canvas(width=200, height=200)
 image = PhotoImage(file= 'logo.png')
-
 canvas.create_image(100, 100, image = image)
 canvas.grid(row=0, column=1)
 
 # Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-emailun_label = Label(text="Email/Username:")
-emailun_label.grid(row=2, column=0)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
 pw_label = Label(text="Password:")
 pw_label.grid(row=3, column=0)
 
 # Entries
 web_entry = Entry(width=35)
 web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.focus()
 email_entry = Entry(width=35)
+email_entry.insert(0, 'ozairmohammad12@gmail.com')
 email_entry.grid(row=2, column=1, columnspan=2)
-pw_entry = Entry(width=21)
-pw_entry.grid(row=3, column=1, columnspan=2)
+pw_entry = Entry(width=30)
+pw_entry.grid(row=3, column=1)
 
 # Buttons
-generate_pw = Button(text="Generate Password")
+generate_pw = Button(text="Generate Password", command= password_generator)
 generate_pw.grid(row=3, column=2)
-add = Button(text="Add", width=36)
+add = Button(text="Add", width=36, command=save)
 add.grid(row=4, column=1, columnspan=2)
-
 
 
 
