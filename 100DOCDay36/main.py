691aaa1587b307e7ffee95e49197cfb743616a68
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-04 14:41:32 +0500

    Stock API Alert Complete

--- 100DOCDay36/main.py
+++ 100DOCDay36/main.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime, timedelta 
-from apik import STOCK_API_KEY, NEWS_API_KEY
-
+from apik import STOCK_API_KEY, NEWS_API_KEY, TWILIO_AUTH_TOKEN, TWILIO_SID
+from twilio.rest import Client
 
 
 stock_api_params = {
@@ -10,21 +10,17 @@ stock_api_params = {
     "apikey": STOCK_API_KEY
 }
 
-news_api_params = {
-    "q":"Tesla Stock Price Today",
-    "from":str((datetime.now() - timedelta(1))).split(" ")[0],
-    "apiKey":NEWS_API_KEY
-}
+
 
 stocks_respone = requests.get(url='https://www.alphavantage.co/query', params=stock_api_params)
-news_response = requests.get(url = "https://newsapi.org/v2/everything", params=news_api_params)
+
 
 stocks_respone.raise_for_status()
-stock_price = stocks_respone.json()
-#print(stock_price)
-print(f"{news_response.json()['articles']}")
+stock_price = stocks_respone.json()['Time Series (Daily)']
+
+
 yesterday = datetime.now() - timedelta(1)
-db_yesterday  = yesterday - timedelta(1) - timedelta(1) - timedelta(1) - timedelta(1) - timedelta(1) 
+db_yesterday  = yesterday - timedelta(1) 
 
 yesterday_key, db_yesterday_key = str(yesterday).split(" ")[0], str(db_yesterday).split(" ")[0]
 
@@ -32,4 +28,33 @@ yesterday_key, db_yesterday_key = str(yesterday).split(" ")[0], str(db_yesterday
 yest_cv = stock_price[yesterday_key]['4. close']
 db_yest_cv = stock_price[db_yesterday_key]['4. close']
 
-percent_diff = ((yest_cv - db_yest_cv) / ((yest_cv + db_yest_cv) / 2)) * 100
+percent_diff = ((float(yest_cv) - float(db_yest_cv)) / ((float(yest_cv) + float(db_yest_cv)) / 2)) * 100
+
+up_down = None
+
+if percent_diff > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+
+
+if abs(percent_diff) > 4:
+    news_api_params = {
+    "q":"TESLA",
+    "from":str((datetime.now() - timedelta(1))).split(" ")[0],
+    "searchIn": "title",
+    "language":"en",
+    "sortBy":"popularity",
+    "apiKey":NEWS_API_KEY
+    }
+
+    news_response = requests.get(url = "https://newsapi.org/v2/everything", params=news_api_params)
+    for article in news_response.json()['articles'][:3]:        
+        client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+        client.messages.create(
+            body=f"TSLA: {round(abs(percent_diff), 2)}{up_down}%\nHeadline:{article['title']}\nBrief:{article['description']}",
+            from_= '+16179345452',
+            to="+923130460101"
+        )
+    
