14e834577424cb958bff25be3e5f7de4e2a5f2c8
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-06 00:48:28 +0500

    refactor variables

--- 100DOCDay39/notification_manager.py
+++ 100DOCDay39/notification_manager.py
@@ -1,15 +1,12 @@
 from twilio.rest import Client
 import os
-TWILIO_SID = os.environ.get("TWILIO_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
-
 class NotificationManager:
     def __init__(self) -> None:
-            self.twilio_sid = TWILIO_SID
-            self.twilio_auth_token = TWILIO_AUTH_TOKEN
+            self.twilio_sid = os.environ.get("TWILIO_SID")
+            self.twilio_auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
     def send_sms(self, price, city_to, city_code_to, travel_from):  
-        client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+        client = Client(self.twilio_sid, self.twilio_auth_token)
         self.message = client.messages.create(
             body= f"Low price alert! Only PKR{price} to fly from Karachi-{travel_from} to {city_to}-{city_code_to}",
             from_= "+16179345452",
