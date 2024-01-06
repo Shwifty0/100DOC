68ac1cd36ff8687916fe3cff61c00dff7fbe8bab
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-06 16:48:03 +0500

    Skipping Day 40 - bored with APIs

---
+++ 100DOCDay40/main.py
@@ -0,0 +1,6 @@
+"""
+This file is empty because I was done using APIs,
+I understand how they work now and can use them in my apps.
+What I am struggling with is making complex applications with just APIs
+
+"""
