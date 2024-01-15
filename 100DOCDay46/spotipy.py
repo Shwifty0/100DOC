b035ff824c03aa267d84c3d9d736d23133e112f2
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-15 19:35:40 +0500

    CHAOS has been initiated

---
+++ 100DOCDay46/spotipy.py
@@ -0,0 +1,11 @@
+"""
+I accidentally deleted the interpreter I was using this whole time.
+I have brought upon a new set of challenges for myself now
+
+1. First do a clean install of Python
+2. Set up a new environment for 100-doc-code
+3. Install the dependecies and start working on the project.
+
+Note: The streak is going on but no real coding was done... I'm sad cus my routine is not an optimal one
+I'm going to priortize my sleep today. and wake up early.
+"""
