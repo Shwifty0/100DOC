23bbcadc6b8ca53b6385580f4aadd64d3a3d6614
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-16 09:35:36 +0500

    Selenium Web-driver

---
+++ 100DOCDay48/main.py
@@ -0,0 +1,9 @@
+# Selenium Web Driver
+from selenium import webdriver
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(url="https://www.google.com") 
