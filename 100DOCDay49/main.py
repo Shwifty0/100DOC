5317e97f3d6688bc7f72452f1d77723660988cca
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-19 08:42:12 +0500

    Saving jobs automated with Selenium

--- 100DOCDay49/main.py
+++ 100DOCDay49/main.py
@@ -5,35 +5,48 @@ import time
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
-
+driver.maximize_window()
 email = "YOUR EMAIL"
-password = "YOUR PW"
+password = "YOUR PASSWORD"
 
-URL = "https://www.linkedin.com/jobs/search/?currentJobId=3806661769&f_AL=true&keywords=Python%20Developer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true"
+# You can change the URL to whatever job postings you want to save
+URL = "https://www.linkedin.com/jobs/search/?currentJobId=3808263564&f_AL=true&geoId=101165590&keywords=Python%20Developer&location=United%20Kingdom&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true"
 driver.get(URL)
 time.sleep(4)
+
+# Click Sign-In option:
 sign_in = driver.find_elements(By.CSS_SELECTOR, value="div a")[-2]
 sign_in.click()
+
+# Fill username and password
 fill_username = driver.find_element(By.ID, value="username").send_keys(email)
 fill_password = driver.find_element(By.ID, value="password").send_keys(password)
+
+# Sign in with the username and password
 second_sign_in = driver.find_element(By.CSS_SELECTOR, value="form div button")
 second_sign_in.click()
 
-
-jobs_list_container = driver.find_elements(By.ID, value="ember193")
-apply_button = driver.find_element(By.CSS_SELECTOR, value="div.mt5 button")
-apply_button.click()
-
-submit_button = driver.find_element(By.CSS_SELECTOR, value = "form.pt4 footer button")
-time.sleep(5)
-submit_button.click()
-# for item in jobs_list_container:
-#     print(item.)
+# Function that clicks the save button on the job posting.
+def save_job():
+    save_button = driver.find_element(
+    By.CLASS_NAME,
+    "jobs-save-button",
+    )
+    save_button.click()
 
 
+# By Pass Captcha:
+input("Press enter to continue the script after completing the captcha: ")
 
 
+# Find all the jobs
+jobs_list_container = driver.find_elements(
+    By.CLASS_NAME, value="job-card-container--clickable"
+)
 
-
-#time.sleep(10)
-
+# Loop trough the jobs and save them rather than applying.
+for item in jobs_list_container:
+    item.click()
+    time.sleep(3)
+    save_job()
+    time.sleep(3)
