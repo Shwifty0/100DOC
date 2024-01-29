689c83ba5956ee13cefd18c146ce5e4b576df217
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-29 14:19:33 +0500

    Library DB Server

--- 100DOCDay63/main.py
+++ 100DOCDay63/main.py
@@ -23,12 +23,14 @@ class Books(db.Model):
 with app.app_context():
     db.create_all()
 
+
+# Home page
 @app.route('/')
 def home():
     books = Books.query.all()
     return render_template("index.html", all_books = books)
 
-
+# Add book page
 @app.route("/add", methods = ["GET", "POST"])
 def add():
     if request.method == "POST":
@@ -40,7 +42,26 @@ def add():
         return redirect(url_for("home"))
     return render_template("add.html")
 
+# Edit Rating Page
+@app.route("/edit/<id>", methods = ["GET", "POST"])
+def edit(id):
+    if request.method == "POST":
+        book_to_update = db.get_or_404(Books, id)
+        print(book_to_update)
+        book_to_update.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_selected = db.get_or_404(Books, id)
+    return render_template("edit_rating.html", book = book_selected)
+
+# Delete Book:
+@app.route("/<id>")
+def delete(id):
+    book_to_delete = db.get_or_404(Books, id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
-
