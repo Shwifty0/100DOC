654824e401f04ac083c2852591c71500b7d97837
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-01-30 16:12:23 +0500

    Integrated TMDB Api, Added functionality for Adding new Movies & Sort them by Rating

--- 100DOCDay64/main.py
+++ 100DOCDay64/main.py
@@ -21,13 +21,13 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 # CREATE TABLE
 class Movies(db.Model):
-    id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    id:Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     title:Mapped[int] = mapped_column(String, unique=True)
     year:Mapped[int] = mapped_column(Integer)
     description:Mapped[int] = mapped_column(String)
-    rating:Mapped[int] = mapped_column(Float)
-    ranking:Mapped[int] = mapped_column(Integer)
-    review:Mapped[int] = mapped_column(String)
+    rating:Mapped[int] = mapped_column(Float, nullable=True)
+    ranking:Mapped[int] = mapped_column(Integer,nullable=True)
+    review:Mapped[int] = mapped_column(String, nullable=True)
     img_url:Mapped[int] = mapped_column(String)
 
 # Edit Rating and Review FlaskForm
@@ -39,32 +39,58 @@ class UpdateForm(FlaskForm):
 # Add Movie Form
 class AddForm(FlaskForm):
     title = StringField(label="Search Movie by Title", validators=[DataRequired()])
+    add_movie = SubmitField(label="Add Movie", validators=[DataRequired()])
 
 with app.app_context():
     db.create_all()
 
-# with app.app_context():
-#     new_movie = Movies(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-#     db.session.add(new_movie)
-#     db.session.commit()
-
 @app.route("/")
 def home():
-    movies = Movies.query.all()
-    return render_template("index.html", list_of_movies = movies)
+    movies = db.session.execute(db.select(Movies).order_by(Movies.rating)).scalars() # orderby sorts in ascending by default
+    ranked_movies = movies.all()
+    for i in range(len(ranked_movies)):
+        ranked_movies[i].ranking = len(ranked_movies) - i
+    db.session.commit()
+        
+    return render_template("index.html", list_of_movies = ranked_movies)
 
 
 @app.route("/add", methods = ["GET", "POST"])
 def add_movie():
     add_form = AddForm()
+    search_movie_endpoint = "https://api.themoviedb.org/3/search/movie"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTM0NDllODdhYTcyNDJlOTQwYzY4MWEzYmFkMDIwMCIsInN1YiI6IjY0YWU5MTM4ZTI0YjkzNWIzMWM5OTc4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9WIwc_M6LdpEBNkrUCxJuB11EmrDRi3ZnIaTWHE2gag"
+    }
+    params = {
+        "query": add_form.data["title"],
+        "include_adult" : True,
+    }
+    
+    if request.method == "POST":
+        response = requests.get(search_movie_endpoint, params=params, headers=headers)
+        return render_template("select.html", api_response = response.json()["results"])
+    
+    if request.args:
+        selected_movie = request.args['movie_id']
+        get_movie_details_endpoint = f"https://api.themoviedb.org/3/movie/{selected_movie}"
+        response = requests.get(get_movie_details_endpoint, headers=headers)
+        title = response.json()["original_title"]
+        img_url = f"https://image.tmdb.org/t/p/w500{response.json()['poster_path']}"
+        description = response.json()["overview"]
+        year = response.json()["release_date"].split("-")[0]
+
+        new_movie = Movies(title=title,
+                            img_url=img_url,
+                            description=description,
+                            year=year
+                        )
+        db.session.add(new_movie)
+        db.session.commit()
+
+        return redirect(url_for("update", id = new_movie.id))
+        
     return render_template("add.html", form = add_form)
 
 @app.route("/edit", methods = ["GET", "POST"])
@@ -76,6 +102,8 @@ def update():
         movie_to_update.review = update_form.data["review"]
         db.session.commit()
         return redirect(url_for("home"))
+
+        
     return render_template("edit.html", form = update_form)
 
 @app.route("/<id>")
