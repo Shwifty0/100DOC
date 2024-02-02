dcfe5da94a858cc6fd3f3027e10674537ebc173b
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-02-03 01:35:50 +0500

    Add blog post fnctionality

--- 100DOCDay66/main.py
+++ 100DOCDay66/main.py
@@ -7,12 +7,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
-from datetime import date
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+ckeditor = CKEditor(app)
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
@@ -32,7 +33,15 @@ class BlogPost(db.Model):
     author: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
-
+# CREATE FORM
+class BlogForm(FlaskForm):
+    title = StringField(label="Enter the title of your Blog Post", validators=[DataRequired()])
+    subtitle = StringField(label="Enter a subtitle for your blogpost", validators=[DataRequired()])
+    author = StringField(label="Enter the name of author", validators=[DataRequired()])
+    body = CKEditorField(label="Edit your blog", validators=[DataRequired()])
+    img_url = StringField(label="Enter an img url", validators=[DataRequired(), URL(require_tld = True)])
+    submit = SubmitField(label="Add blog")
+    
 with app.app_context():
     db.create_all()
 
@@ -53,9 +62,34 @@ def show_post(post_id):
 
 
 # TODO: add_new_post() to create a new blog post
-@app.route("/add_new_post", methods = ["POST"])
+@app.route("/new-post", methods = ["GET", "POST"])
+def add_new_post():
+    add_blog_post = BlogForm()
+    print(add_blog_post.data)
+    if add_blog_post.validate_on_submit():
+        date = datetime.now().strftime('%B %d, %Y')
+        print(date)
+        title = add_blog_post.data["title"]
+        subtitle = add_blog_post.data["subtitle"]
+        author = add_blog_post.data["author"]
+        body = add_blog_post.data["body"]
+        img_url = add_blog_post.data["img_url"]
+        new_blog_post = BlogPost(title = title,
+                                author = author,
+                                body = body,
+                                subtitle = subtitle,
+                                date = date,
+                                img_url = img_url)
+        db.session.add(new_blog_post)
+        db.session.commit()
+        print("Successfully added the blog post")
+
+    return render_template("make-post.html", form = add_blog_post)
+
 # TODO: edit_post() to change an existing blog post
-@app.route("/edit_post", methods = ["PUT"])
+@app.route("/edit_post", methods = ["GET", "PUT"])
+def edit_post():
+    
 # TODO: delete_post() to remove a blog post from the database
 @app.route("/delete_post")
 # Below is the code from previous lessons. No changes needed.
