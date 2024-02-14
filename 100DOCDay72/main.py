da5bafd76209125a7105a1372d25f0cc1aebc664
Muhammad Ozair <102323082+Shwifty0@users.noreply.github.com>
2024-02-15 03:41:50 +0500

    Renamed file

---
+++ 100DOCDay72/main.py
@@ -0,0 +1,10 @@
+"""
+The remaining part of the course deals with Data Science and Machine Learning.
+All of the code is executed in Google Colab.
+What I am going to be doing is complete the remaining course on Google Colab,
+and for daily updates I'll create commits just like this one to keep the streak going.
+
+
+1: Revisiting Concepts of Pandas Library.
+
+"""
